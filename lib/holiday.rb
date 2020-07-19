429ba751fde284940a7a495944aa918c45c05fe0
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 13:27:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
 holiday_hash[:season][holiday_name] = supply_array
 end
-end
+
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].map do |key,value|
