6c93305b1df6973eb6149db6eedb378524a5af03
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 12:47:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,6 +36,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
  # binding.pry
   holiday_hash[:winter].each do |key, value|
     #binding.pry
+    
 end
 end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
